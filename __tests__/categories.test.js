91ebc8b046e8f8fc262a96c4697b35eefd8915ac
Earl Jay Caoile <unfie555@gmail.com>
2020-01-15 20:58:28 -0800

    passing tests using Categories object and comparing value types with validator

--- __tests__/categories.test.js
+++ __tests__/categories.test.js
@@ -34,44 +34,16 @@ describe('Categories Model', () => {
       });
   });
 
-  it('has a number value for id and a string value for name', () => {
-    // const personRules = {
-    //   fields: {
-    //     schema: {
-    //       type: 'object', required: true, 
-    //       {
-    //         id: { type: 'number', required: true },
-    //         name: { type: 'string', required: true}
-    //       }
-    //     },
-    //     firstName: { type: 'string', required: true },
-    //     lastName: { type: 'string', required: true },
-    //     hair: {
-    //       type: 'object', required: true,
-    //       type: { typing: 'string', required: true },
-    //       color: { typing: 'string', required: true },
-    //     },
-    //     favoriteFoods: { type: 'array', valueType: 'string' },
-    //     married: { typtypeing: 'boolean', required: true },
-    //     kids: { type: 'number', required: true },
-    //   },
-    // };
-
+  it('contains values that have the same types defined in the class schema', () => {
     let obj = { name: 'Test Category' };
     return categories.create(obj)
       .then(record => {
-        // console.log('Record value: ', record);
         return categories.get(record._id)
           .then(category => {
-            expect()
-            console.log('Category[0] value: ', category[0]);
-            // Category[0] value:  { name: 'Test Category', id: 'ba40b7df-21eb-439f-9d7f-adef51ddde09' }
-            // expect(validator.isCategories(category)).toEqual(true);
             Object.keys(obj).forEach(key => {
-              Categories.s
-              // console.log('Category[0][key] value: ', category[0][key]);
-              // console.log('Category[0].type value: ', category[0].type);
-              // expect(validator.isCorrectType(category[0][key], category[0].type));
+              console.log('Category[0][key] value: ', category[0][key]);
+              console.log('categories.schema[key].type value: ', categories.schema[key].type);
+              expect(validator.isCorrectType(category[0][key], categories.schema[key].type)).toEqual(true);
             });
           });
         });
